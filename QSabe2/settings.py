55b09e5769a77eaffdfc7eef6992f72872191130
Marlon Ferrari <mfmarlonferrari@gmail.com>
2014-07-18 16:51:33 -0300

    melhorado login

--- QSabe2/settings.py
+++ QSabe2/settings.py
@@ -15,7 +15,7 @@ TEMPLATE_DIRS = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
+LOGIN_REDIRECT_URL = '/recomendadas'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'qa6re6z%i19(mx*(chswc&me1w1gq0$@6ts#h*m7!^1s+p1in%'
 
